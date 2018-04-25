bd3555f5bea6639fb35413a83baf4c175fed1726
nicholasadamou <nicholasadamouemail@gmail.com>
2018-04-25 18:54:43 -0400

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Onion Pi
+Onion Pi [![Build Status](https://travis-ci.org/nicholasadamou/OnionPi.svg?branch=master)](https://travis-ci.org/nicholasadamou/OnionPi)
 ========
 ![logo](logo.png)
 
@@ -18,7 +18,6 @@ Requirements
 
 Onion Pi Requires:
 
-* Raspbian Jessie ([PIXEL](https://www.raspberrypi.org/downloads/raspbian/))
 * Two WiFi Cards (e.g. On-board chip + [TL-WN725N](https://www.amazon.com/gp/product/B008IFXQFU/ref=oh_aui_detailpage_o03_s00?ie=UTF8&psc=1))
 * Micro-USB to USB 2.0/3.0 converter (e.g. [USB to Micro-USB Charge & Sync Cable](https://www.amazon.com/gp/product/B00SVVY844/ref=oh_aui_detailpage_o05_s00?ie=UTF8&psc=1))
 * Portable Battery Bank (e.g. [Anker PowerCore 5000](https://www.amazon.com/gp/product/B01CU1EC6Y/ref=oh_aui_detailpage_o02_s00?ie=UTF8&psc=1))
@@ -31,7 +30,12 @@ Install
 Download, review, then execute the script(s):
 
 ```
-git clone git://github.com/NicholasAdamou/OnionPi.git && cd OnionPi && ./pifi.sh && ./tor.sh
+git clone git://github.com/NicholasAdamou/OnionPi.git \
+    && cd OnionPi \
+    && wget -O setup_pifi.sh https://raw.githubusercontent.com/nicholasadamou/PiFi/master/src/setup.sh \
+    && chmod +x setup_pifi.sh \
+    && ./setup_pifi.sh \
+    && ./src/setup.sh
 ```
 
 Follow the on-screen directions.
@@ -49,7 +53,7 @@ More Information
 License
 -------
 
-Onion Pi is © 2017 Nicholas Adamou.
+Onion Pi is © 2018 Nicholas Adamou.
 
 It is free software, and may be redistributed under the terms specified in the [LICENSE] file.
 
---
+++ src/setup.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+# Based on Adafruit Learning Technologies Onion Pi project
+# see: http://learn.adafruit.com/onion-pi
+
+declare BASH_UTILS_URL="https://raw.githubusercontent.com/nicholasadamou/bash-utils/master/utils.sh"
+
+declare skipQuestions=false
+
+trap "exit 1" TERM
+export TOP_PID=$$
+
+declare APP_NAME="Onion Pi"
+declare MONIKER="4d4m0u"
+
+declare STATION=wlan1
+declare AP=wlan0
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+setup_onion_pi() {
+  echo "$(tput setaf 2)
+                     ..
+                    ,:
+            .      ::
+            .:    :2.
+             .:,  1L
+              .v: Z, ..::,
+               :k:N.Lv:
+                22ukL
+                JSYk.$(tput bold ; tput setaf 7)
+               ,B@B@i
+               BO@@B@.
+             :B@L@Bv:@7
+           .PB@iBB@  .@Mi
+         .P@B@iE@@r  . 7B@i
+        5@@B@:NB@1$(tput setaf 5) r  ri:$(tput bold ; tput setaf 7)7@M
+      .@B@BG.OB@B$(tput setaf 5)  ,.. .i, $(tput bold ; tput setaf 7)MB,
+      @B@BO.B@@B$(tput setaf 5)  i7777,    $(tput bold ; tput setaf 7)MB.
+     PB@B@.OB@BE$(tput setaf 5)  LririL,.L. $(tput bold ; tput setaf 7)@P
+     B@B@5iB@B@i$(tput setaf 5)  :77r7L, L7 $(tput bold ; tput setaf 7)O@
+     @B1B27@B@B,$(tput setaf 5) . .:ii.  r7 $(tput bold ; tput setaf 7)BB
+     O@.@M:B@B@:$(tput setaf 5) v7:    ::.  $(tput bold ; tput setaf 7)BM
+     :Br7@L5B@BO$(tput setaf 5) irL: :v7L. $(tput bold ; tput setaf 7)P@,
+      7@,Y@UqB@B7$(tput setaf 5) ir ,L;r: $(tput bold ; tput setaf 7)u@7
+       r@LiBMBB@Bu$(tput setaf 5)   rr:.$(tput bold ; tput setaf 7):B@i
+         FNL1NB@@@@:   ;OBX
+           rLu2ZB@B@@XqG7$(tput sgr0 ; tput setaf 2)
+              . rJuv::
+
+              $(tput setaf 2)"$AP"p_name
+      $(tput bold ; tput setaf 5)by adafruit$(tput sgr0)
+         $(tput bold ; tput setaf 5)modified by $MONIKER$(tput sgr0)
+  "
+
+  echo "$(tput setaf 6)This script will configure your Raspberry Pi as an Onion Pi Tor proxy.$(tput sgr0)"
+  
+  if [ "$TRAVIS" != "true" ]; then
+      read -p "$(tput bold ; tput setaf 2)Press [Enter] to begin, [Ctrl-C] to abort...$(tput sgr0)"
+  fi
+
+  update
+  upgrade
+
+  declare -a PKGS=(
+      "tor"
+      "macchanger"
+      "iptables-persistent"
+  )
+
+  for PKG in "${PKGS[@]}"; do
+      install_package "$PKG" "$PKG"
+  done
+
+  FILE=/etc/tor/torrc
+  cp "$FILE" "$FILE".bak
+  cat > "$FILE" <<- EOL
+  Log notice file /var/log/tor/notices.log
+  VirtualAddrNetwork 10.192.0.0/10
+  AutomapHostsSuffixes .onion,.exit
+  AutomapHostsOnResolve 1
+  TransPort 9040
+  TransListenAddress 192.168.42.1
+  DNSPort 53
+  DNSListenAddress 192.168.42.1
+EOL
+
+  iptables -F
+  iptables -t nat -F
+
+  iptables -t nat -A PREROUTING -i "$AP" -p tcp --dport 22 -j REDIRECT --to-ports 22
+  iptables -t nat -A PREROUTING -i "$AP" -p udp --dport 53 -j REDIRECT --to-ports 53
+  iptables -t nat -A PREROUTING -i "$AP" -p tcp --syn -j REDIRECT --to-ports 9040
+
+  sh -c "iptables-save > /etc/iptables.ipv4.nat"
+  sudo systemctl enable netfilter-persistent
+
+  FILE=/var/log/tor/notices.log
+  touch "$FILE"
+  chown debian-tor "$FILE"
+  chmod 644 "$FILE"
+
+  service tor start
+  update-rc.d tor enable
+}
+
+restart() {
+    ask_for_confirmation "Do you want to restart?"
+    
+    if answer_is_yes; then
+        sudo shutdown -r now &> /dev/null
+    fi
+}
+
+main() {
+    # Ensure that the following actions
+    # are made relative to this file's path.
+
+    cd "$(dirname "${BASH_SOURCE[0]}")" \
+        && source <(curl -s "$BASH_UTILS_URL") \
+        || exit 1
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    skip_questions "$@" \
+        && skipQuestions=true
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    ask_for_sudo
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    setup_onion_pi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    if ! $skipQuestions; then
+        restart
+    fi
+}
+
+main "$@"
